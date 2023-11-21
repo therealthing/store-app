f1b4d761407489dc6124c163af7950af08630363
Dan Jipa <dan.jipa@cognizant.com>
2023-11-21 10:31:36 +0200

    adding pagination

--- app/screens/ViewAllProductsScreen.tsx
+++ app/screens/ViewAllProductsScreen.tsx
@@ -1,6 +1,7 @@
 import { useNavigation } from '@react-navigation/native';
 import * as React from 'react';
-import {FlatList, SafeAreaView, Text, View} from 'react-native';
+import { useState } from 'react';
+import {Alert, Button, FlatList, SafeAreaView, Text, View} from 'react-native';
 import useProducts from '../useAllProducts';
 import { ProductItem, ProductItemType } from '../components/ProductItem';
 import styled from 'styled-components/native';
@@ -14,30 +15,59 @@ const ViewAllProductsScreen: React.FC = function() {
     const renderItem = ({item}: ProductItem) => (
       <ProductItem {...item} onPress={() => navigation.navigate("SingleProduct", {id: item.id})}/>
     )
-
-    const { data, isLoading, isSuccess, isError, error }  = useProducts();
+    const [page,setPage] = useState<number>(0);
+    const { data, isLoading, isSuccess, isError, error, isFetching }  = useProducts(page);
+    
 
     if(error) {
       console.error(error);
     }
-    
+
     return (
       <SafeAreaView>
         {isLoading && (<View><Text>Loading</Text></View>)}
         {isError && (<View><Text>Error loading product..</Text></View>)}
-        <Container>
+        <PageContainer>
         {isSuccess && (
-          <FlatList 
-            data={data}
-            renderItem={renderItem}
-          />)}
-        </Container>
+            <>
+              <PageControlsContainer>
+                <PageController onPress={() => page > 0 ? setPage(page - 1) : Alert.alert('no more products') } title="Previous page" />
+                <Button onPress={() => setPage(page + 1) } title="Next page" />
+              </PageControlsContainer>
+              <ProductListContainer>
+                <FlatList
+                    data={data}
+                    renderItem={renderItem} />
+              </ProductListContainer>
+              </>
+              )}
+        </PageContainer>
       </SafeAreaView>
     )
   };
+const ProductListContainer = styled.View`
+  flex-basis:100%;
+`;
 
- const Container = styled.View`
+const PageControlsContainer = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  flex-basis: 40px;
+  line-height: 40px;
+`;
+
+const PageController = styled.Button`
+  display: flex; 
+  font-size: 12px;
+  padding: 0 4px 0 4px;
+  color: darkgray;
+`;
+
+ const PageContainer = styled.View`
   background-color: lightgray;
+  display: flex;
+  flex-direction: column;
   padding: 4px;
  `;
 
--- app/useAllProducts.ts
+++ app/useAllProducts.ts
@@ -1,11 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
 import { LIST_ENDPOINT, requestOptions } from './constants';
 
-const fetchProducts = async () => {
-    const { products } = await fetch(LIST_ENDPOINT, requestOptions).then(data => data.json());
+const fetchProducts = async (page:number) => {
+    const limit = 10;
+    const skip = page*limit;
+    const LIST_ENDPOINT_PAGED = `${LIST_ENDPOINT}?limit=${limit}&skip=${skip}`;
+    const { products } = await fetch(LIST_ENDPOINT_PAGED, requestOptions).then(data => data.json());
     return products;
 };
 
-const useAllProducts = () => useQuery({ queryKey: ['products'], queryFn: fetchProducts, networkMode: 'offlineFirst'});
+const useAllProducts = (page:number) => useQuery({ queryKey: ['/products', page], queryFn: () => fetchProducts(page), networkMode: 'offlineFirst'});
 
 export default useAllProducts;
